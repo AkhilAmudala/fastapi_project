9660bdc824be4ffabb46797e7c92c41401ed4461
Akhil Amudala <amudalaakhil333@gmail.com>
2023-12-16 11:15:06 -0800

    redis connection & adding middleware

--- inventory/main.py
+++ inventory/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
-
+from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+redis= get_redis_connection(
+    host='redis-15735.c323.us-east-1-2.ec2.cloud.redislabs.com:15735', #publicendpoint_of_DB
+    password='yM42M5CXGmNvhM2L7oLlZuzJEmNlUOTr',
+    port='11844',
+    decode_responses=True
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -9,4 +25,3 @@ async def root():
 #to start server:- "uvicorn main:app --reload"
 #to list all running processes in port 8000:- "lsof -i:8000"
 #to kill a process:- "kill -9 PID"
-
