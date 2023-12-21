661ab3afbf583fee552b0650df2c5faa25d39ba1
Akhil Amudala <amudalaakhil333@gmail.com>
2023-12-20 16:14:48 -0800

    redis connection changes

--- inventory/main.py
+++ inventory/main.py
@@ -1,23 +1,32 @@
 from fastapi import FastAPI
-from redis_om import get_redis_connection, HashModel
+from redis import RedisError
+from redis_om import get_redis_connection, HashModel, redis
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException
+from pydantic import BaseModel
+
+
 app = FastAPI()
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['http://localhost:8000'],
     allow_methods=['*'],
     allow_headers=['*']
 )
 
+import redis
 
-redis= get_redis_connection(
-    host='redis-15735.c323.us-east-1-2.ec2.cloud.redislabs.com:15735', #publicendpoint_of_DB
-    password='yM42M5CXGmNvhM2L7oLlZuzJEmNlUOTr',
-    port='11844',
-    decode_responses=True
-)
+r = redis.Redis(
+  host='redis-15735.c323.us-east-1-2.ec2.cloud.redislabs.com', port=15735, password='yM42M5CXGmNvhM2L7oLlZuzJEmNlUOTr',decode_responses=True)
+
+# redis= get_redis_connection(
+#     host='redis-15735.c323.us-east-1-2.ec2.cloud.redislabs.com:15735', #publicendpoint_of_DB
+#     password='yM42M5CXGmNvhM2L7oLlZuzJEmNlUOTr',
+#     port='15735', #get this from redis DB connection: https://app.redislabs.com/#/databases/12058748/subscription/2220215/view-bdb/configuration
+#     decode_responses=True
+# )
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -30,9 +39,19 @@ class Product(HashModel):
     #to connect product class to redis database -> Meta class
 
     class Meta:
-        database=redis
+        database=r
 #now whatever product we create will be stored in redis        
 
-#to start server:- "uvicorn main:app --reload"
+@app.get('/products')
+def all():
+    return [format(i) for i in Product.all_pks()] #to return all products in  redis
+
+@app.post('/products')
+def create(product: Product):
+    product.save()
+    return {"message": "Product created successfully"}
+
+
+#to start server from main fodler directory:- "uvicorn main:app --reload"
 #to list all running processes in port 8000:- "lsof -i:8000"
 #to kill a process:- "kill -9 PID"
